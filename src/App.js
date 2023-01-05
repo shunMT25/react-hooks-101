e934ddd0fd19ea78aa10ddf8d47374b38fd4aa6d
shunMT25 <120574907+shunMT25@users.noreply.github.com>
2023-01-05 23:14:54 +0900

    counter for useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  console.log(count);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const increment2 = () => setCount(a => a + 1);
+
   return (
-    <div>
-      aaa
-    </div>
+    <React.Fragment>
+    <div> count: {count} </div>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    <button onClick={increment2}>+1</button>
+    </React.Fragment>
   );
 }
 
