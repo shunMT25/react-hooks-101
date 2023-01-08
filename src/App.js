fbc12308194910ab67dbea1af23bd845b568b975
shunMT25 <120574907+shunMT25@users.noreply.github.com>
2023-01-08 15:00:47 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const App = props => {
 
   const[state,setState] = useState(props)
   const{name, price} = state
 
+  useEffect(() => {
+    console.log("useEffect")
+  })
+
+  useEffect(() => {
+    console.log("useEffect2")
+  }, [name])
+
+  const renderPeriod = () => {
+    console.log("render Piriod")
+    return "。"
+  }
+
   return (
     <React.Fragment>
-      <p>現在の{name}は、{price}です。</p>
+      <p>現在の{name}は、{price}です{renderPeriod()}</p>
       <button onClick={() => setState({...state, price: price + 1})}>+1</button>
       <button onClick={() => setState({...state, price: price - 1})}>-1</button>
       <button onClick={() => setState(props)}>Reset</button>
